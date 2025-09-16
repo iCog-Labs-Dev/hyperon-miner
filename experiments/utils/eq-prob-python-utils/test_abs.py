deff5b94b3624ed035ebb0658839aded3f1bade0
tinsaeabreham45 <tinsaeabreham45@gmail.com>
2025-09-16 10:50:24 +0300

    test the connected_subpatterns function logic

--- experiments/utils/eq-prob-python-utils/test_abs.py
+++ experiments/utils/eq-prob-python-utils/test_abs.py
@@ -6,12 +6,13 @@ from eq_prob_update import *
 # var = "$x"
 
 # pattern = ["Human", "$x", "$y", "Likes", "$x", "$z"]
-# partition = [
-#     [["Human", "$x", "$y"]],      # Block 1: $x, $y
-#     [["Likes", "$x", "$z"]],      # Block 2: $x, $z  
-#     [["Smart", "$y"]]             # Block 3: $y
-# ]
+partition = [
+    [["Human", "$x", "$y"], ["Smart", "$y"]],      # Block 1: $x, $y
+    [["Human", "$x", "$z"]],      # Block 2: $x, $z  
+    [["Human", "$y"]]             # Block 3: $y
+]
 
 # print("is Left Block more abstract:", is_blk_more_abstract(l_blk, r_blk,var))
 # print("Sorted by abstraction:", sort_by_abstraction(l_blk + r_blk, var)) 
 # print("The Joint Variable: ", joint_variables(pattern, partition))
+print(connected_subpatterns_with_var(partition, "$x"))
